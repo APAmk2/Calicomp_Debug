c13afffc90c6d14f604259a70906b4c3986954ae
git <APAmk2@users.noreply.github.com>
2023-10-07 19:40:55 +0400

    Ка-ли-ко-мп, менее черновая реализация (наканецта!11!1!!)

--- resources/scenes/Calicomp.gd
+++ resources/scenes/Calicomp.gd
@@ -2,36 +2,72 @@ extends Node
 
 var karmotrine: int = 0
 var adelhyde: int = 0
-var flanerhyde: int = 0
+var flanergide: int = 0
 var bronson: int = 0
 var delta: int = 0
 
+func _input(event):
+	if Input.is_action_just_pressed("decreaseAldehyde"):
+		adelhyde -= 1
+		adelhyde = clamp(adelhyde, 0, 20)
+		CalcFalseVis(adelhyde, "adelhyde")
+	if Input.is_action_just_pressed("decreaseBronson"):
+		bronson -= 1
+		bronson = clamp(bronson, 0, 20)
+		CalcFalseVis(bronson, "bronson")
+	if Input.is_action_just_pressed("decreaseDelta"):
+		delta -= 1
+		delta = clamp(delta, 0, 20)
+		CalcFalseVis(delta, "delta")
+	if Input.is_action_just_pressed("decreaseFlanergide"):
+		flanergide -= 1
+		flanergide = clamp(flanergide, 0, 20)
+		CalcFalseVis(flanergide, "flanergide")
+	if Input.is_action_just_pressed("decreaseKarmotrine"):
+		karmotrine -= 1
+		karmotrine = clamp(karmotrine, 0, 20)
+		CalcFalseVis(karmotrine, "karmotrine")
+
+func CalcFalseVis(numForRevert, ingName):
+	var revertNum = 20 - numForRevert
+	var totalRevertNum = 20 - GetTotalIgredientsNum()
+	for i in range(20, 20 - revertNum , -1):
+		if(i > 0 && i <= 10):
+			get_node("2D/" + ingName + "/slot" + str(i)).modulate = "383838"
+		else:
+			get_node("2D/" + ingName + "/slot" + str(i)).visible = false
+	for i in range(20, 20 - totalRevertNum , -1):
+		if(i > 0):
+			get_node("2D/totalIngs/slot" + str(i)).visible = false
+
 func _on_area_2d_area_entered(area):
 	$"2D/shaker".texture = load("res://resources/Export_Sprites/shake_hover_spr_0.png") 
-	if(area == $"2D/karmotrine/Area2D"):
-		$"2D/karmotrine".inShaker = true
-	if(area == $"2D/adelhyde/Area2D"):
-		$"2D/adelhyde".inShaker = true
-	if(area == $"2D/flanerhyde/Area2D"):
-		$"2D/flanerhyde".inShaker = true
-	if(area == $"2D/bronson/Area2D"):
-		$"2D/bronson".inShaker = true
-	if(area == $"2D/delta/Area2D"):
-		$"2D/delta".inShaker = true
+	if(area == $"2D/igredients/karmotrine/Area2D"):
+		$"2D/igredients/karmotrine".inShaker = true
+	if(area == $"2D/igredients/adelhyde/Area2D"):
+		$"2D/igredients/adelhyde".inShaker = true
+	if(area == $"2D/igredients/flanergide/Area2D"):
+		$"2D/igredients/flanergide".inShaker = true
+	if(area == $"2D/igredients/bronson/Area2D"):
+		$"2D/igredients/bronson".inShaker = true
+	if(area == $"2D/igredients/delta/Area2D"):
+		$"2D/igredients/delta".inShaker = true
 
 func _on_area_2d_area_exited(area):
 	$"2D/shaker".texture = load("res://resources/Export_Sprites/shaker_spr_0.png")
-	if(area == $"2D/karmotrine/Area2D"):
-		$"2D/karmotrine".inShaker = false
-	if(area == $"2D/adelhyde/Area2D"):
-		$"2D/adelhyde".inShaker = false
-	if(area == $"2D/flanerhyde/Area2D"):
-		$"2D/flanerhyde".inShaker = false
-	if(area == $"2D/bronson/Area2D"):
-		$"2D/bronson".inShaker = false
-	if(area == $"2D/delta/Area2D"):
-		$"2D/delta".inShaker = false
+	if(area == $"2D/igredients/karmotrine/Area2D"):
+		$"2D/igredients/karmotrine".inShaker = false
+	if(area == $"2D/igredients/adelhyde/Area2D"):
+		$"2D/igredients/adelhyde".inShaker = false
+	if(area == $"2D/igredients/flanergide/Area2D"):
+		$"2D/igredients/flanergide".inShaker = false
+	if(area == $"2D/igredients/bronson/Area2D"):
+		$"2D/igredients/bronson".inShaker = false
+	if(area == $"2D/igredients/delta/Area2D"):
+		$"2D/igredients/delta".inShaker = false
 
+func GetTotalIgredientsNum():
+	return karmotrine + adelhyde + flanergide + bronson + delta
 
 func EmitSound(mode):
 	if(mode):
--- resources/scenes/igredient.gd
+++ resources/scenes/igredient.gd
@@ -7,7 +7,7 @@ var dragging = false
 var inShaker = false
 
 func Grabbed(viewport, event, shape_idx):
-	if Input.is_action_just_pressed("LMB"):
+	if (Input.is_action_just_pressed("LMB") && $"../../..".GetTotalIgredientsNum() < 20):
 		dragging = true
 
 func _physics_process(delta):
@@ -17,24 +17,53 @@ func _physics_process(delta):
 			global_position.y = clamp(global_position.y, 226, 498)
 	else:
 		if(inShaker):
-			$"../../sfx".stream = load("res://resources/Exported_Sounds/audiogroup_default/addingredient.ogg")
-			$"../../sfx".play()
+			$"../../../sfx".stream = load("res://resources/Exported_Sounds/audiogroup_default/addingredient.ogg")
+			$"../../../sfx".play()
 			match igredient_name:
 				"karmotrine":
-					$"../..".karmotrine += 1
-					print($"../..".karmotrine)
+					$"../../..".karmotrine += 1
+					for i in range(1, $"../../..".karmotrine + 1):
+						if(i <= 10):
+							get_node("../../karmotrine/slot" + str(i)).modulate = "ffffff"
+						else:
+							get_node("../../karmotrine/slot" + str(i)).visible = true
+					print($"../../..".karmotrine)
 				"adelhyde":
-					$"../..".adelhyde += 1
-					print($"../..".adelhyde)
-				"flanerhyde":
-					$"../..".flanerhyde += 1
-					print($"../..".flanerhyde)
+					$"../../..".adelhyde += 1
+					for i in range(1, $"../../..".adelhyde + 1):
+						if(i <= 10):
+							get_node("../../adelhyde/slot" + str(i)).modulate = "ffffff"
+						else:
+							get_node("../../adelhyde/slot" + str(i)).visible = true
+					print($"../../..".adelhyde)
+				"flanergide":
+					$"../../..".flanergide += 1
+					for i in range(1, $"../../..".flanergide + 1):
+						if(i <= 10):
+							get_node("../../flanergide/slot" + str(i)).modulate = "ffffff"
+						else:
+							get_node("../../flanergide/slot" + str(i)).visible = true
+					print($"../../..".flanergide)
 				"bronson":
-					$"../..".bronson += 1
-					print($"../..".bronson)
+					$"../../..".bronson += 1
+					for i in range(1, $"../../..".bronson + 1):
+						if(i <= 10):
+							get_node("../../bronson/slot" + str(i)).modulate = "ffffff"
+						else:
+							get_node("../../bronson/slot" + str(i)).visible = true
+					print($"../../..".bronson)
 				"delta":
-					$"../..".delta += 1
-					print($"../..".delta)
+					$"../../..".delta += 1
+					for i in range(1, $"../../..".delta + 1):
+						if(i <= 10):
+							get_node("../../delta/slot" + str(i)).modulate = "ffffff"
+						else:
+							get_node("../../delta/slot" + str(i)).visible = true
+						print($"../../..".delta)
+			for i in range(1, $"../../..".GetTotalIgredientsNum() + 1):
+				if(i > 0):
+					get_node("../../totalIngs/slot" + str(i)).visible = true
+					
 		global_position = rest_pos
 func _input(event):
 	if event is InputEventMouseButton:
